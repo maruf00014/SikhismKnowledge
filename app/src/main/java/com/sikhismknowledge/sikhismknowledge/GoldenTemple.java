336f1ec1deadfbe015ab2d4105b6534beb1d3161
maruf00014 <maruf00014@gmail.com>
2018-05-19 16:26:25 +0600

    goldentemple updated

--- app/src/main/java/com/sikhismknowledge/sikhismknowledge/GoldenTemple.java
+++ app/src/main/java/com/sikhismknowledge/sikhismknowledge/GoldenTemple.java
@@ -1,7 +1,10 @@
 package com.sikhismknowledge.sikhismknowledge;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
@@ -9,8 +12,11 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+import com.sikhismknowledge.sikhismknowledge.SikHistory.Other;
 
 public class GoldenTemple extends AppCompatActivity {
+    Button ArtandArchitecture,Maryada,Jalau,MaharajaRanjitSingh,AkalTakhtSahib,
+            BerBabaBuddhaJi,Kaulsar,DukhbhanjaniBeri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +24,81 @@ public class GoldenTemple extends AppCompatActivity {
         setContentView(R.layout.activity_golden_temple);
 
 
-        // adds
 
-     MobileAds.initialize(this,getString(R.string.app_ad_id));
-        AdView adView = findViewById(R.id.webAdView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        adView.loadAd(adRequest);
+
+        ArtandArchitecture = findViewById(R.id.ArtandArchitecture);
+        ArtandArchitecture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GoldenTemple.this,com.sikhismknowledge.sikhismknowledge.goldenTemple.ArtandArchitecture.class);
+                startActivity(intent);
+            }
+        });
+
+        Maryada = findViewById(R.id.Maryada);
+        Maryada.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GoldenTemple.this, com.sikhismknowledge.sikhismknowledge.goldenTemple.Maryada.class);
+                startActivity(intent);
+            }
+        });
+
+        Jalau = findViewById(R.id.Jalau);
+        Jalau.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GoldenTemple.this, com.sikhismknowledge.sikhismknowledge.goldenTemple.Jalau.class);
+                startActivity(intent);
+            }
+        });
+
+        MaharajaRanjitSingh = findViewById(R.id.MaharajaRanjitSingh);
+        MaharajaRanjitSingh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GoldenTemple.this, com.sikhismknowledge.sikhismknowledge.goldenTemple.MaharajaRanjitSingh.class);
+                startActivity(intent);
+            }
+        });
+
+        AkalTakhtSahib = findViewById(R.id.AkalTakhtSahib);
+        AkalTakhtSahib.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GoldenTemple.this, com.sikhismknowledge.sikhismknowledge.goldenTemple.AkalTakhtSahib.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        BerBabaBuddhaJi = findViewById(R.id.BerBabaBuddhaJi);
+        BerBabaBuddhaJi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GoldenTemple.this,com.sikhismknowledge.sikhismknowledge.goldenTemple.BerBabaBuddhaJi.class);
+                startActivity(intent);
+            }
+        });
+
+        Kaulsar = findViewById(R.id.Kaulsar);
+        Kaulsar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GoldenTemple.this,com.sikhismknowledge.sikhismknowledge.goldenTemple.Kaulsar.class);
+                startActivity(intent);
+            }
+        });
+
+        DukhbhanjaniBeri = findViewById(R.id.DukhbhanjaniBeri);
+        DukhbhanjaniBeri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GoldenTemple.this,com.sikhismknowledge.sikhismknowledge.goldenTemple.DukhbhanjaniBeri.class);
+                startActivity(intent);
+            }
+        });
 
 
 
---
+++ app/src/main/java/com/sikhismknowledge/sikhismknowledge/goldenTemple/BerBabaBuddhaJi.java
@@ -0,0 +1,93 @@
+package com.sikhismknowledge.sikhismknowledge.goldenTemple;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.sikhismknowledge.sikhismknowledge.R;
+
+/**
+ * Created by Maruf on 5/19/2018.
+ */
+
+public class BerBabaBuddhaJi extends AppCompatActivity {
+    private WebView webview;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.web_view);
+
+
+        if(isNetworkStatusAvialable (getApplicationContext())) {
+
+        } else {
+            Toast.makeText(getApplicationContext(), "No Internet Connection!", Toast.LENGTH_LONG).show();
+            finish();
+
+        }
+
+        progressBar = findViewById(R.id.progressBar);
+
+        webview = findViewById(R.id.webView);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setLoadsImagesAutomatically(true);
+        webview.setWebViewClient(new WebViewClient());
+
+        webview.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                progressBar.setProgress(progress);
+                if (progress > 55) {
+                    progressBar.setVisibility(View.GONE);
+
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+
+                }
+            }
+        });
+        webview.loadUrl("http://www.sikhismknowledge.com/golden-temple/gurudwaras-around-golden-temple/ber-baba-buddha-ji/");
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (webview.canGoBack()) {
+                        webview.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public static boolean isNetworkStatusAvialable (Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null)
+        {
+            NetworkInfo netInfos = connectivityManager.getActiveNetworkInfo();
+            if(netInfos != null)
+                if(netInfos.isConnected())
+                    return true;
+        }
+        return false;
+    }
+
+
+}
